edd401c91cc76d6efffc7411b6ec49118ae87d04
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-23 17:47:08 +0500

    Update PostList.vue

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -6,18 +6,7 @@
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there!"
       previewText="This my first post!" />
-    <PostPreview
-      id="2"
-      :is-admin="isAdmin"
-      thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      title="Hello there - the second time!"
-      previewText="This my second post!" />
-    <PostPreview
-      id="3"
-      :is-admin="isAdmin"
-      thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      title="Hi!"
-      previewText="This my third post!" />
+
   </section>
 </template>
 
@@ -32,6 +21,10 @@ export default {
     isAdmin: {
       type: Boolean,
       default: false
+    },
+     posts: {
+      type: Array,
+      require: true
     }
   }
 }
